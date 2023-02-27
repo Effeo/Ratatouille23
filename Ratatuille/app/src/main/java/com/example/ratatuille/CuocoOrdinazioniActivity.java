49ef40a979d548bd9980160f003ffe100a711a9f
MarcoPastazio <96430070+MarcoPastazio@users.noreply.github.com>
2023-02-27 15:38:41 +0100

    from desktop

---
+++ Ratatuille/app/src/main/java/com/example/ratatuille/CuocoOrdinazioniActivity.java
@@ -0,0 +1,14 @@
+package com.example.ratatuille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CuocoOrdinazioniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuoco_ordinazioni);
+    }
+}
---
+++ Ratatuille/app/src/main/java/com/example/ratatuille/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.example.ratatuille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button login_b = (Button) findViewById(R.id.button_1);
+        Button torna_indietro_b = (Button) findViewById(R.id.button_2);
+
+        //L'amministratore va direttamente sulla sua schermata principale
+
+
+        login_b.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent finestraCambioPassword = new Intent(view.getContext(), CambioPasswordActivity.class);
+                startActivity(finestraCambioPassword);
+            }
+        });
+
+        torna_indietro_b.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent finestraWelcome = new Intent(view.getContext(), MainActivity.class);
+                startActivity(finestraWelcome);
+            }
+        });
+
+
+    }
+
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        //This is used to hide/show 'Status Bar' & 'System Bar'. Swip bar to get it as visible.
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+
+}
