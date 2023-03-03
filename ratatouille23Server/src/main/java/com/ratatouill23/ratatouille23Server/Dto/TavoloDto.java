63dfb03c4cc5e5c8eae37d2a9331e979774949e5
Francesco Orlando <91286116+Effeo@users.noreply.github.com>
2023-03-03 12:40:55 +0100

    desktop

---
+++ ratatouille23Server/src/main/java/com/ratatouill23/ratatouille23Server/Dto/TavoloDto.java
@@ -0,0 +1,21 @@
+package com.ratatouill23.ratatouille23Server.Dto;
+
+public class TavoloDto {
+
+    private int id_tavolo;
+
+    public TavoloDto(int id_tavolo) {
+        this.id_tavolo = id_tavolo;
+    }
+
+    public int getId_tavolo() {
+        return id_tavolo;
+    }
+
+    public void setId_tavolo(int id_tavolo) {
+        this.id_tavolo = id_tavolo;
+    }
+
+    public TavoloDto() {
+    }
+}
