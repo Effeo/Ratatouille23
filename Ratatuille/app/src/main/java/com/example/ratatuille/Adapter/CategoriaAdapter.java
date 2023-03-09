fa77bfaec94dafa7e5c9c51ec5065e617ae954cd
MarcoPastazio <96430070+MarcoPastazio@users.noreply.github.com>
2023-03-09 12:09:23 +0100

    from desktop

---
+++ Ratatuille/app/src/main/java/com/example/ratatuille/Adapter/CategoriaAdapter.java
@@ -0,0 +1,26 @@
+package com.example.ratatuille.Adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoriaAdapter  extends RecyclerView.Adapter<CategoriaAdapter.CategoriaHolder> {
+
+
+    @NonNull
+    @Override
+    public CategoriaAdapter.CategoriaHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoriaAdapter.CategoriaHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
