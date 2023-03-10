e2f23a5168680d456c0a8abfd17fc3a5f5d19ae1
MarcoPastazio <96430070+MarcoPastazio@users.noreply.github.com>
2023-03-10 12:01:30 +0100

    from desktop

--- Ratatuille/app/src/main/java/com/example/ratatuille/Service/Interface/IPiattoService.java
+++
@@ -1,9 +0,0 @@
-package com.example.ratatuille.Service.Interface;
-
-import com.example.ratatuille.Service.Callback;
-
-public interface IPiattoService {
-    public void findPiattoById(Callback callback, int id_piatto);
-
-    public void getAllPiatti(Callback callback);
-}
