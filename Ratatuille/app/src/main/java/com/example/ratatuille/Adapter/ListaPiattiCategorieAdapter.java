e90dcef08693de3e262709ca6deab4abcfc609a9
MarcoPastazio <96430070+MarcoPastazio@users.noreply.github.com>
2023-03-14 16:27:47 +0100

    from desktop

--- Ratatuille/app/src/main/java/com/example/ratatuille/Adapter/ListaPiattiCategorieAdapter.java
+++ Ratatuille/app/src/main/java/com/example/ratatuille/Adapter/ListaPiattiCategorieAdapter.java
@@ -39,18 +39,25 @@ public class ListaPiattiCategorieAdapter extends RecyclerView.Adapter<ListaPiatt
     public void onBindViewHolder(@NonNull ListaPiattiCategorieAdapter.ListaPiattiCategorieHolder holder, int position) {
 
         holder.nomePiatto.setText(piatti.get(position).toUpperCase());
+        String quantita = (String) holder.quantita_per_piatto.getText();
 
         holder.button_meno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //lo faccio dopo
+                int quantitanum = Integer.parseInt(quantita);
+                quantitanum++;
+                holder.quantita_per_piatto.setText(String.valueOf(quantitanum));
             }
         });
 
         holder.button_piu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //lo faccio dopo
+                int quantitanum = Integer.parseInt(quantita);
+                if(quantitanum != 0){
+                    quantitanum--;
+                    holder.quantita_per_piatto.setText(String.valueOf(quantitanum));
+                }
             }
         });
     }
