4d0295ec67acd132ea2ac51b382c6d7d245b043d
Francesco Orlando <91286116+Effeo@users.noreply.github.com>
2023-03-23 14:28:09 +0100

    des

--- Ratatuille/app/src/main/java/com/example/ratatuille/View/AdminStatisticheActivity.java
+++ Ratatuille/app/src/main/java/com/example/ratatuille/View/AdminStatisticheActivity.java
@@ -2,16 +2,47 @@ package com.example.ratatuille.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ratatuille.Presenter.UtentePresenter;
 import com.example.ratatuille.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AdminStatisticheActivity extends AppCompatActivity {
     private UtentePresenter utentePresenter;
+
     private AdminStatisticheActivity adminStatisticheActivity;
+
+    public TextView textIncassoMedio;
+    public TextView textValoreMedioConto;
+    public TextView textIncassoComplessivo;
+
+    public Spinner spinnerAnnoInizio;
+    public Spinner spinnerMeseInizio;
+    public Spinner spinnerGiornoInizio;
+
+    public Spinner spinnerAnnoFine;
+    public Spinner spinnerMeseFine;
+    public Spinner spinnerGiornoFine;
+
+    public String anno_inizio;
+    public String mese_inizio;
+    public String giorno_inizio;
+
+    public String anno_fine;
+    public String mese_fine;
+    public String giorno_fine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +57,50 @@ public class AdminStatisticheActivity extends AppCompatActivity {
         ImageButton btn_admin_messaggi = (ImageButton) findViewById(R.id.admin_messaggi);
         ImageButton btn_admin_statistiche = (ImageButton) findViewById(R.id.admin_statistiche);
         ImageButton btn_admin_add_utente = (ImageButton) findViewById(R.id.admin_add_utente);
+
+        Button btn_calcola = (Button) findViewById(R.id.button_calcola);
+
+        textIncassoMedio = (TextView) findViewById(R.id.incasso_medio);
+        textValoreMedioConto = (TextView) findViewById(R.id.incasso_medio);
+        textIncassoComplessivo = (TextView) findViewById(R.id.incasso_complessivo);
+
+        spinnerAnnoInizio = (Spinner) findViewById(R.id.spinner_anno_di_inizio);
+        spinnerMeseInizio = (Spinner) findViewById(R.id.spinner_mese_di_inizio);
+        spinnerGiornoInizio = (Spinner) findViewById(R.id.spinner_giorno_di_inizio);
+
+        spinnerAnnoFine = (Spinner) findViewById(R.id.spinner_anno_di_fine);
+        spinnerMeseFine = (Spinner) findViewById(R.id.spinner_mese_di_fine);
+        spinnerGiornoFine = (Spinner) findViewById(R.id.spinner_giorno_di_fine);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.anni_inizio, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAnnoInizio.setAdapter(adapter);
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.mesi_inizio, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMeseInizio.setAdapter(adapter2);
+
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.giorni_inizio, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGiornoInizio.setAdapter(adapter3);
+
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
+                R.array.anni_fine, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAnnoFine.setAdapter(adapter4);
+
+        ArrayAdapter<CharSequence> adapter5 = ArrayAdapter.createFromResource(this,
+                R.array.mesi_fine, android.R.layout.simple_spinner_item);
+        adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMeseFine.setAdapter(adapter5);
+
+        ArrayAdapter<CharSequence> adapter6 = ArrayAdapter.createFromResource(this,
+                R.array.giorni_fine, android.R.layout.simple_spinner_item);
+        adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerGiornoFine.setAdapter(adapter6);
 
         adminStatisticheActivity = this;
 
@@ -70,6 +145,17 @@ public class AdminStatisticheActivity extends AppCompatActivity {
                 utentePresenter.goAdminAddUtente(adminStatisticheActivity);
             }
         });
+
+        btn_calcola.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkDate()){
+                    System.out.println("Tutto ok");
+                }
+                else
+                    Toast.makeText(adminStatisticheActivity.getApplicationContext(), "Le date non sono valide", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void onWindowFocusChanged(boolean hasFocus) {
@@ -85,5 +171,77 @@ public class AdminStatisticheActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
+    }
+
+    public boolean checkDate(){
+        boolean flag = false;
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat dateFormat = new
+                SimpleDateFormat("yyyy-MM-dd");
+
+        anno_inizio = spinnerAnnoInizio.getSelectedItem().toString();
+        mese_inizio = spinnerMeseInizio.getSelectedItem().toString();
+        giorno_inizio = spinnerGiornoInizio.getSelectedItem().toString();
+
+        anno_fine = spinnerAnnoFine.getSelectedItem().toString();
+        mese_fine = spinnerMeseFine.getSelectedItem().toString();
+        giorno_fine = spinnerGiornoFine.getSelectedItem().toString();
+
+        try {
+            Date dataInizio = dateFormat.parse(anno_inizio + "-" + mese_inizio + "-" + giorno_inizio);
+            Date dataFine = dateFormat.parse(anno_fine + "-" + mese_fine + "-" + giorno_fine);
+
+            if(dataInizio.before(dataFine)){
+                if(checkNovembre(1) && checkNovembre(0) && checkFebbraio(1) && checkFebbraio(0)){
+                    flag = true;
+                }
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return flag;
+    }
+
+    public boolean checkNovembre(int i){
+        boolean flag = true;
+
+        if(i == 1){
+            if(mese_inizio.equals("11") || mese_inizio.equals("04") || mese_inizio.equals("06") || mese_inizio.equals("09")) {
+                if(giorno_inizio.equals("31")) flag = false;
+            }
+        }
+        else{
+            if(mese_fine.equals("11") || mese_fine.equals("04") || mese_fine.equals("06") || mese_fine.equals("09")) {
+                if(giorno_fine.equals("31")) flag = false;
+            }
+        }
+
+        return flag;
+    }
+
+    public boolean checkFebbraio(int i){
+        boolean flag = true;
+
+        if(i == 1){
+            if(mese_inizio.equals("02")){
+                if(giorno_inizio.equals("30") || giorno_inizio.equals("31") || (!checkBisestile(Integer.parseInt(anno_inizio)) && giorno_inizio.equals("29"))){
+                    flag = false;
+                }
+            }
+        }
+        else{
+            if(mese_fine.equals("02")){
+                if(giorno_fine.equals("30") || giorno_fine.equals("31") || (!checkBisestile(Integer.parseInt(anno_fine)) && giorno_fine.equals("29"))){
+                    flag = false;
+                }
+            }
+        }
+
+        return flag;
+    }
+
+    public boolean checkBisestile(int anno){
+        return (anno % 4 == 0 && anno % 100 != 0) || (anno % 400 == 0);
     }
 }
